de7be74f18c982e0e980226f7e212d9109593166
Raymond Kam <rkam@connected.io>
2020-11-23 13:55:47 -0500

    Initial Setup

---
+++ UIKitWithSwiftUI/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  UIKitWithSwiftUI
+//
+//  Created by cl-dev on 2020-11-23.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitWithSwiftUI/SwiftUIContainerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIContainerViewController.swift
+//  UIKitWithSwiftUI
+//
+//  Created by cl-dev on 2020-11-23.
+//
+
+import UIKit
+
+class SwiftUIContainerViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
